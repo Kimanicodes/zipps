14f111e3724e9c8d9f1e307f43a3aa366e92df72
Gisumwa Mairura <gisumwa@gmail.com>
2016-06-15 19:52:06 +0300

    Initial commit, kindly pull it and do your sections, cheers

---
+++ Student_details.py
@@ -0,0 +1,5 @@
+def student_average():
+    student_dict={range(n):grades}
+    for k,v in student_dict.iteritems:
+        total=[sum(v) for v in student_dict]
+        print("{0} : {1}".format(v,total)
