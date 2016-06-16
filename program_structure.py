b48c4cab7b53340a49ed6dc710b953934ece30e1
Morris Kimani <mzeemorris@gmail.com>
2016-06-16 17:41:29 +0300

    Adds the file program_structure.py that outlines the structure of our group

---
+++ program_structure.py
@@ -0,0 +1,43 @@
+class Student(object):
+	"""The Student class"""
+	def __init__(self, name, eng, kis, mat):
+		"""The method should:
+				-accept and store a student's name and the score of the 
+				student in the 3 subjects
+				-initialize the variables: average and rank
+
+		  MORRIS and HILLARY
+		"""
+		self.name = name
+		self.eng = eng
+		self.mat = mat
+		self.average = None
+		self.rank = None
+		self.studentID = None
+
+	def average(self):
+		"""The method should:
+				-calculate average mark scored by the student and store this in the
+				average variables
+
+			GISUMWA
+		"""
+
+	def grade(self):
+		"""The method should give the student a certain grade A, B, C etc. based on a certain criteria
+
+		LOICE-decide on the grading criteria"""
+
+#the function below is not part of class Student
+def rank(list_of_student_objects):
+	"""Sort the list of students and store the rank of each student in the rank variable"""
+
+
+def main():
+ 	"""This will be the main logic of our program. It will control the flow
+ 	Program execcution will start here. We can discuss this as a group
+ 	"""
+
+
+
+
